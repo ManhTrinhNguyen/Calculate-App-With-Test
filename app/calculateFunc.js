9bdde706c700d8d466583148e47044e8cc96c7f6
Trinh Nguyen <trinhnguyen@Trinhs-MBP.cable.rcn.com>
2023-12-09 09:52:37 -0500

    Fix function to remove insignificant trailing zeros numbers

--- app/calculateFunc.js
+++ app/calculateFunc.js
@@ -10,7 +10,14 @@ function calculateFunc(n1, operator, n2) {
   } else if (operator === "divide") {
     result = parseFloat(n1) / parseFloat(n2);
   }
-  return result;
+
+  if (result.toString().includes(".")) {
+    return Number(parseFloat(result).toFixed(1)) 
+  } else {
+    return result
+  }
 }
+
+console.log(calculateFunc(-2, 'multiply', -2));
 
 module.exports = calculateFunc;
